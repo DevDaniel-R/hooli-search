ccf0ca0a9a179962885a7acde26e95dc7d73e860
Daniel Rodriguez <drodriguez661@gmail.com>
2019-10-26 07:55:04 -0500

    start of mockup. super simple, using google logo as a placeholder

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>hooli</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>hooli</title>
 </head>
+
 <body>
 
-<div class="mainSection">
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+        </div>
 
+        <div class="searchContainer">
 
+            <input class="searchBox" type="text" name="term">
+            <input class="searchButton" type="submit" value="search">
 
- <div>
-  
+        </div>
+
+    </div>
+
 </body>
+
 </html>

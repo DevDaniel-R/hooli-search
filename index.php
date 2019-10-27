e9272f23a621747d821c51696e3c7d219f205629
Daniel Rodriguez <drodriguez661@gmail.com>
2019-10-27 08:39:30 -0500

    styling home page CSS

--- index.php
+++ index.php
@@ -6,24 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>hooli</title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
 </head>
 
 <body>
 
-    <div class="mainSection">
-        <div class="logoContainer">
-            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+    <div class="wrapper indexPage">
+
+        <div class="mainSection">
+            <div class="logoContainer">
+                <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+            </div>
+
+            <div class="searchContainer">
+
+                <form action="search.php" method="GET">
+                    <input class="searchBox" type="text" name="term">
+                    <input class="searchButton" type="submit" value="search">
+                </form>
+
+            </div>
+
         </div>
-
-        <div class="searchContainer">
-
-            <form action="search.php" method="GET">
-                <input class="searchBox" type="text" name="term">
-                <input class="searchButton" type="submit" value="search">
-            </form>
-
-        </div>
-
     </div>
 
 </body>

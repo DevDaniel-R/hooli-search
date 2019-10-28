5d912ed65243720f9c6188aaef4e7e1201b07390
Daniel Rodriguez <drodriguez661@gmail.com>
2019-10-27 19:48:11 -0500

    home page styled

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
         <div class="mainSection">
             <div class="logoContainer">
-                <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+                <img src="assets/images/hoolie.png">
             </div>
 
             <div class="searchContainer">

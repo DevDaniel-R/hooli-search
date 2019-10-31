f89b7ea64dd4100eeea60d267a24bda193dcd91c
Daniel Rodriguez <drodriguez661@gmail.com>
2019-10-30 19:32:47 -0500

    styling search page

--- search.php
+++ search.php
@@ -1,3 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo $_GET["term"];
+<head>
+    <title>hooli</title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+</head>
+
+<body>
+
+    <div class="wrapper">
+
+        <div class="header">
+
+            <div class="headerContent">
+
+                <div class="logoContainer">
+                    <a href="index.php">
+                        <img src="assets/images/hoolie.png">
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+
+                        <div class="searchBarContainer">
+
+                            <input class="searchBox" type="text" name="term">
+                            <button>
+                                Search
+                            </button>
+
+                        </div>
+
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>

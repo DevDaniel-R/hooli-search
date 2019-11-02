a1c4f34c47d3fcc8616b5f90c21802db727d6c85
Daniel Rodriguez <drodriguez661@gmail.com>
2019-11-02 10:01:15 -0500

    finished most of the styling, now starting crawler

--- search.php
+++ search.php
@@ -1,3 +1,15 @@
+<?php
+
+    if(isset($_GET["term"])) {
+    $term = $_GET["term"];
+    } else {
+        exit("You must enter a search term");
+    }
+
+    $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -37,11 +49,23 @@
 
             </div>
 
+            <div class="tabsContainer">
+
+                <ul class="tabList">
+                    <li class="<?php echo $type == 'sites' ? 'active' : ''  ?>">
+                        <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+                            Searches
+                        </a>
+                    </li>
+                    <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+                            Images
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-
-
     </div>
-
 </body>
 
 </html>

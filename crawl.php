511ad2e115db368ee2801d244abc9f7fd2072871
Daniel Rodriguez <drodriguez661@gmail.com>
2019-11-07 23:06:29 -0600

    convert relative links to absolute links

--- crawl.php
+++ crawl.php
@@ -1,6 +1,20 @@
 <?php
-
 include("classes/DomDocumentParser.php");
+
+function createLink($src, $url)
+{
+
+  $scheme = parse_url($url)["scheme"]; //http
+  $host = parse_url($url)["host"]; // www.reecekenney.com
+
+  if (substr($src, 0, 2) == "//") {
+    $src = $scheme . ":" . $src;
+  } else if (substr($src, 0, 1) == "/") {
+    $src = $scheme . "://" . $host . $src;
+  }
+
+  return $src;
+}
 
 function followLinks($url)
 {
@@ -11,11 +25,17 @@ function followLinks($url)
   foreach ($linkList as $link) {
     $href = $link->getAttribute("href");
 
-    if (strpos($href, "#") !== false) { }
+    if (strpos($href, "#") !== false) {
+      continue;
+    } else if (substr($href, 0, 11) == "javascript:") {
+      continue;
+    }
+
+    createLink($href, $url);
 
     echo $href . "<br>";
   }
 }
 
-$startUrl = "https://www.apple.com";
+$startUrl = "https://www.bbc.com";
 followLinks($startUrl);

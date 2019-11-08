4b0e0587bb691f6110f42b71e02a2dd80749fe72
Daniel Rodriguez <drodriguez661@gmail.com>
2019-11-07 23:46:50 -0600

    converted relative links. starting recursive crawler function.

--- crawl.php
+++ crawl.php
@@ -11,6 +11,12 @@ function createLink($src, $url)
     $src = $scheme . ":" . $src;
   } else if (substr($src, 0, 1) == "/") {
     $src = $scheme . "://" . $host . $src;
+  } else if (substr($src, 0, 2) == "./") {
+    $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+  } else if (substr($src, 0, 3) == "../") {
+    $src = $scheme . "://" . $host . "/" . $src;
+  } else if (substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+    $src = $scheme . "://" . $host . "/" . $src;
   }
 
   return $src;
@@ -31,7 +37,7 @@ function followLinks($url)
       continue;
     }
 
-    createLink($href, $url);
+    $href = createLink($href, $url);
 
     echo $href . "<br>";
   }
